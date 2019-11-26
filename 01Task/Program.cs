0470c094a04653e6945b2128f33e26f741749dac
Hannah Karpukhina <HannahK@intellis.io>
2019-11-26 13:06:07 +0200

    added area of triangle

--- 01Task/Program.cs
+++ 01Task/Program.cs
@@ -18,6 +18,10 @@ namespace _01Task
             double c = Convert.ToDouble(Console.ReadLine());
             double perimeter = a + b + c;
             Console.WriteLine("perimeter = {0}", perimeter);
+            Console.Write("height = ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            //double area;
+            Console.WriteLine("area = {0}", (b * height) / 2);
             Console.ReadLine();
         }
     }

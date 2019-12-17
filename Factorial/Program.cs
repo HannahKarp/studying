ef729fdbcfe459b94a186005690b80f68950e5c5
Hannah Karpukhina <HannahK@intellis.io>
2019-12-17 23:15:24 +0200

    created factorial function

---
+++ Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        public static int Factorial(int input)
+        {
+            int factorial = 1;
+            for (int i = 1; i < input; i++)
+            {
+                factorial = (i + 1) * factorial;
+            }
+            return factorial;
+        }
+  
+
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("number of factorial = ");
+            number = Convert.ToInt32(Console.ReadLine());
+            number = Factorial(number);
+            Console.WriteLine(number);
+            Console.ReadLine();
+        }
+        
+    }
+}

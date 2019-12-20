f17ec185d82ed54478eccbd6d0e73a663f8a2725
Hannah Karpukhina <HannahK@intellis.io>
2019-12-20 07:57:15 +0200

    Added new even and odd numbers function

---
+++ EvenNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EvenNumbers
+{
+    class Program
+    {
+        public static int EvenNumbers(int input)
+        {
+            int number = 1;
+            if (number > 0)
+                for (int i = 1; i <= input; i++)
+                {
+                    if (i % 2 == 0)
+                        Console.WriteLine($"even numbers = " + i);
+                    else
+                        Console.WriteLine($"odd numbers = " + i);
+                }
+            else
+                Console.WriteLine("specified number < = 1");
+            return number;
+        }
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("specified number = ");
+            number = Convert.ToInt32(Console.ReadLine());
+            number = EvenNumbers(number);
+            Console.WriteLine(number);
+            Console.ReadLine();
+        }
+    }
+}
